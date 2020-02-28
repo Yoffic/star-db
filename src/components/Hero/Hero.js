82c791843526c6705c9424d41aa44addad107601
Olga Ioffe <yoffic@yahoo.com>
2020-02-28 18:37:08 +0300

    Add: fetching data from swapi

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,26 +1,53 @@
 import React, { Component } from 'react';
+import SwapiService from '../../services/swapi-service';
 
 import './Hero.css';
 
 export default class Hero extends Component {
+  
+  swapiService = new SwapiService();
+  
+  state = {
+    planet: {},
+  };
+
+  constructor() {
+    super();
+    this.updatePlanet();
+  }
+
+  onPlanetLoaded = (planet) => {
+    this.setState({planet});
+  }
+  
+  updatePlanet() {
+    const id = Math.floor(Math.random() * 19) + 1;
+    this.swapiService
+      .getPlanet(id)
+      .then(this.onPlanetLoaded);
+  }
+
   render() {
+    const { planet: {
+      id, name, population, rotationPeriod, diameter
+    } } = this.state;
     return (
       <div className="jumbotron d-flex hero">
-        <img className="random-img" src={`https://starwars-visualguide.com/assets/img/planets/12.jpg`} alt="" />
+        <img className="random-img" src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} alt="" />
         <div>
-          <h2>Random Item</h2>
+          <h2>{name}</h2>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
-              <span className="term">Some description</span>
-              <span>text</span>
+              <span className="term">Population</span>
+              <span>{population}</span>
             </li>
             <li className="list-group-item">
-              <span className="term">Some description</span>
-              <span>text</span>
+              <span className="term">Rotation Period</span>
+              <span>{rotationPeriod}</span>
             </li>
             <li className="list-group-item">
-              <span className="term">Some description</span>
-              <span>text</span>
+              <span className="term">Diameter</span>
+              <span>{diameter}</span>
             </li>
           </ul>
         </div>

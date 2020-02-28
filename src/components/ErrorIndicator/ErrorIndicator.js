d44111f924c3222c46c3ad2a577e8b9406860cf9
Olga Ioffe <yoffic@yahoo.com>
2020-02-28 23:35:36 +0300

    Add: error indicator

---
+++ src/components/ErrorIndicator/ErrorIndicator.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './ErrorIndicator.css';
+import icon from './death-star.png';
+
+const ErrorIndicator = () => {
+  return (
+    <div className="error-indicator d-flex">
+      <img src={icon} alt="error icon"/>
+      <span className="boom">BOOM! </span>
+      <span>Something has gone terribly wrong </span>
+      <span>(but we have already sent droids to fix it)</span>
+    </div>
+  );
+};
+
+export default ErrorIndicator;

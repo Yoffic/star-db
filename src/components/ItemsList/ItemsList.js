0a9af82d734a9b6c4da6babacea3ff16d4808b6d
Olga Ioffe <yoffic@yahoo.com>
2020-02-29 00:39:00 +0300

    Add: loading data for list and interactivity

--- src/components/ItemsList/ItemsList.js
+++ src/components/ItemsList/ItemsList.js
@@ -1,20 +1,49 @@
 import React, { Component } from 'react';
 
+import SwapiService from '../../services/swapi-service';
+import Spinner from '../Spinner';
+
 import './ItemsList.css';
 
 export default class ItemsList extends Component {
-  render () {
+  swapiService = new SwapiService();
+  state = {
+    peopleList: null,
+  }
+
+  componentDidMount() {
+    this.swapiService
+      .getAllPeople()
+      .then((peopleList) => {
+        this.setState({
+          peopleList
+        });
+      });
+  }
+
+  renderItems(items) {
+    return items.map(({ id, name }) => {
+      return (
+        <li className="list-group-item" 
+            key={id}
+            onClick={ () => this.props.onItemSelected(id) }>
+          {name}
+        </li>
+      );
+    });
+  }
+
+  render() {
+    const { peopleList } = this.state;
+    if (!peopleList) {
+      return <Spinner />;
+    }
+
+    const list = this.renderItems(peopleList);
+
     return (
       <ul className="items-list list-group bg-dark">
-        <li className="list-group-item">
-          Luke Skywalker
-        </li>
-        <li className="list-group-item">
-          Darth Vader
-        </li>
-        <li className="list-group-item">
-          R2-D2
-        </li>
+        { list }
       </ul>
     );
   }

7c2c2f2e32382e8bb6f251aa87e76fc9ff1a36b4
Olga Ioffe <yoffic@yahoo.com>
2020-03-01 16:11:07 +0300

    Refactor: creature view

--- src/components/CreatureView/CreatureView.js
+++ src/components/CreatureView/CreatureView.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 import './CreatureView.css';
 
-const CreatureView = ({ creature, image, children }) => {
+const CreatureView = ({ creature, image, fields }) => {
   const { name } = creature;
+  const dataList = fields.map(({ field, label }) => {
+    return (
+      <li className="list-group-item">
+        <span className="term">{label}: </span>
+        <span>{field}</span>
+      </li>
+    );
+  });
+
   return (
     <>
       <img className="creature-image"
@@ -12,11 +21,7 @@ const CreatureView = ({ creature, image, children }) => {
         <div className="card-body">
           <h4>{name}</h4>
           <ul className="list-group list-group-flush">
-            {
-              React.Children.map(children, (child) => {
-                return React.cloneElement(child, { creature });
-              })
-            }
+            {dataList}
           </ul>
         </div>
     </>

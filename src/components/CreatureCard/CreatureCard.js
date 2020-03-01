67e54787dbe4e719cf0b794d14a6830bb020bfe3
Olga Ioffe <yoffic@yahoo.com>
2020-03-01 16:10:45 +0300

    Update: creature card

--- src/components/CreatureCard/CreatureCard.js
+++ src/components/CreatureCard/CreatureCard.js
@@ -1,88 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './CreatureCard.css';
-import SwapiService from '../../services/swapi-service';
-import Spinner from '../Spinner';
+import CreatureView from '../CreatureView';
 
-export default class CreatureCard extends Component {
-  swapiService = new SwapiService();
+const CreatureCard = (props) => {
+  const { data, image, loading, fields } = props;
+  const creatureData = loading
+    ? props.children
+    : <CreatureView creature={data} image={image} fields={fields}></CreatureView>;
 
-  state = {
-    creature: null,
-    loading: false,
-  };
-
-  componentDidMount() {
-    this.updateCreature();
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.creatureId !== prevProps.creatureId) {
-      this.setState({
-        loading: true
-      });
-      this.updateCreature();
-    }
-  }
-
-  updateCreature() {
-    const { creatureId } = this.props;
-    if (!creatureId) return;
-
-    this.swapiService
-      .getPerson(creatureId)
-      .then((creature) => {
-        this.setState({
-          creature,
-          loading: false,
-        });
-      });
-  }
-
-  render () {
-    if (!this.state.creature) {
-      return <span>Select a person from a list</span>
-    }
-
-    const { creature, loading } = this.state;
-
-    const spinner = loading ? <Spinner /> : null;
-    const creatureData = !loading ? <CreatureView creature={creature}/> : null;
-
-    return (
-      <div className="creature-card card d-flex bg-dark">
-        {spinner}
-        {creatureData}
-      </div>
-    );
-  }
-}
-
-const CreatureView = ({ creature }) => {
-  const { id, name, gender, birthYear, eyeColor } = creature;
-  const imgApi = `https://starwars-visualguide.com/assets/img/characters/`;
   return (
-    <React.Fragment>
-      <img className="creature-image"
-          src={`${imgApi}${id}.jpg`} alt="" />
-
-        <div className="card-body">
-          <h4>{name}</h4>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <span className="term">Gender: </span>
-              <span>{gender}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Birth Year: </span>
-              <span>{birthYear}</span>
-            </li>
-            <li className="list-group-item">
-              <span className="term">Eye Color: </span>
-              <span>{eyeColor}</span>
-            </li>
-          </ul>
-        </div>
-    </React.Fragment>
+    <div className="creature-card card d-flex bg-dark">
+      {creatureData}
+    </div>
   );
 };
+
+export default CreatureCard;

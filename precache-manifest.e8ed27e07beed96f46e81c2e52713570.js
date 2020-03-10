a2ace3539b1fafea2e8e6e7136d9553455119396
Olga Ioffe <yoffic@yahoo.com>
2020-03-10 10:57:28 +0300

    Updates

---
+++ precache-manifest.e8ed27e07beed96f46e81c2e52713570.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97c949305e8e6257a4b19e19949b70c8",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "c3a22037965f3ea6ca51",
+    "url": "/star-db/static/css/main.bacfdd3d.chunk.css"
+  },
+  {
+    "revision": "3677dab4a9e63bf4a415",
+    "url": "/star-db/static/js/2.09a6a809.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/star-db/static/js/2.09a6a809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3a22037965f3ea6ca51",
+    "url": "/star-db/static/js/main.aec5c33e.chunk.js"
+  },
+  {
+    "revision": "7417beba386f58d3e97b",
+    "url": "/star-db/static/js/runtime-main.bb006c70.js"
+  }
+]);
